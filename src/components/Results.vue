7df4a4dcd23adf5868eac0a6835b851d35a0ee76
chtysl <devCihat@gmail.com>
2021-01-10 23:53:22 +0300

    finished wack a mole game

---
+++ src/components/Results.vue
@@ -0,0 +1,15 @@
+<template>
+  <div v-if="score < 300">Super fast {{ score }}ms</div>
+  <div v-else-if="score < 600">Kinda fast {{ score }}ms</div>
+  <div v-else-if="score < 2500">Super slow {{ score }}ms</div>
+  <div v-else>Slow as snail {{ score }}ms</div>
+</template>
+
+<script>
+export default {
+  props: ["score"],
+};
+</script>
+
+<style >
+</style>
